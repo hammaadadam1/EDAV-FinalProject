e1e8baef6d604d1cf40d1c8c6a02b5536df49326
Hammaad Adam <hammaadadam@Hammaad-Adams-MacBook-Pro.local>
2018-12-10 00:17:35 -0500

    Updating Hammaad's stuff

--- HAHeatMap.R
+++ HAHeatMap.R
@@ -7,11 +7,16 @@ access_token <- get_spotify_access_token()
 
 library(tidyverse)
 
-artists <- unique(class_playlists$artist_name)
+data <- read.csv("ClassMusicData.csv")
+class_names <- read.csv("Names.csv")
 
-subset(class_playlists, grepl("_", artist_name)==TRUE)$artist
+data = data %>% dplyr::inner_join(class_names, by = "user_id")
 
-song_data <- class_playlists %>% 
+artists <- unique(data$artist_name)
+
+subset(data, grepl("_", artist_name)==TRUE)$artist
+
+song_data <- data %>% 
               select(-X, -playlist_name, -playlist_uri, 
                              -playlist_img, -playlist_tracks_url, 
                              -playlist_num_tracks, -snapshot_id,
@@ -22,14 +27,14 @@ song_data <- class_playlists %>%
 
 # Can verify actually unique by running View(song_data_unique %>% count(track_name, sort = TRUE))
 
-song_freq <- class_playlists %>% group_by(track_name, artist_name)  %>%
+song_freq <- data %>% group_by(track_name, artist_name)  %>%
               summarise(Freq = n()) %>%
                 arrange(desc(Freq))
 
-artist_freq <- class_playlists %>% count(artist_name, sort = TRUE)
+artist_freq <- data %>% count(artist_name, sort = TRUE)
 View(artist_freq)
 
-artist_per_playlist <- class_playlists %>% distinct(Name, artist_name) %>% 
+artist_per_playlist <- data %>% distinct(Name, artist_name) %>% 
                         count(artist_name, sort = TRUE) %>% 
                           left_join(artist_freq, by="artist_name") %>% 
                               select(artist_name, nplaylists = n.x, nsongs = n.y)
@@ -40,11 +45,11 @@ scatterplot_n <- ggplot(artist_per_playlist, aes(x=nplaylists, y=nsongs)) +
 
 scatterplot_n
 
-artist_by_listener <- class_playlists %>% distinct(Name, artist_name, track_name) %>% 
+artist_by_listener <- data %>% distinct(Name, artist_name, track_name) %>% 
                         group_by(artist_name, Name) %>% 
                           summarise(n = n())
 
-song_user <- class_playlists %>% select(Name, track_name, artist_name) %>% cbind(1) %>% 
+song_user <- data %>% select(Name, track_name, artist_name) %>% cbind(1) %>% 
               rename(binaryid="1") %>% 
                 distinct(Name, artist_name, track_name, .keep_all = TRUE) %>%
                   spread(Name, binaryid, fill = 0) %>%
@@ -60,8 +65,7 @@ for(i in 1:nusers){
   for(j in 2:(nusers+1)){
     if(i+1==j){
       song_intersection[i,j] = 0
-    }
-    else{
+    } else{
       song_intersection[i,j] = nrow(subset(song_user, song_user[,i+2]==1 & song_user[,j+1]==1))
     }
   }
@@ -79,5 +83,3 @@ ggplot(song_intersection, aes(x = Name, y = fct_rev(sharer))) +
     coord_fixed() + theme_heat + scale_x_discrete(position = "top") +
       scale_fill_gradient2(low = "white", mid = "white", high = "darkblue", 
         name = "Number of Shared Songs") + xlab("") + ylab("")
-
-
